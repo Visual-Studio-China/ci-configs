15f8121ac7d05f3cfc8921bb34ca3723e699e040
Qingqing Sun <qisun@microsoft.com>
2016-11-02 16:16:41 +0800

    refactor scripts

--- process_files_and_toc.ps1
+++ process_files_and_toc.ps1
@@ -1,108 +1,40 @@
-# 1. read the file, set or update the metadata
-# 2. resolve related links to format [related link](xref:uid)
-# 3. generate toc
+<#
+.SYNOPSIS
+    This is a Powershell script to process files and generate a toc file.
+.DESCRIPTION
+    This script is used in specific ci projects(appveyor.yml) and depending on both
+    APPVEYOR built-in environment variables and the ones defined in those projects.
+    We didn't decouple it cause we want to keep the update as more as possible in this
+    script instead of in the appveyor.yml.
+#>
 
+# Validate that the repo has only one root folder
 if((ls $env:APPVEYOR_BUILD_FOLDER -dir).count -ne 1)
 {
   $host.SetShouldExit(-1)
 }
-$pattern = "^(?s)\s*[-]{3}(.*?)[-]{3}\r?\n"
 
-$script_block =
-{
-  param($file, $root_name, $pattern)
+$header_pattern = "^(?s)\s*[-]{3}(.*?)[-]{3}\r?\n"
+$landing_page_pattern = "Module\s*Name\s*:"
+$root_path = (ls $env:APPVEYOR_BUILD_FOLDER -dir | select -First 1).FullName
+$root_name = Split-Path $root_path -Leaf
+$toc_path = Join-Path $root_path "toc.yml"
 
-  function set_metadata($header, $new_header, $key, $value, $overwrite)
-  {
-    $meta = "(?m)^$key\s*:[\s\S].*"
-    if($header -match $meta -and $overwrite -eq $true)
-    {
-      $new_header = $new_header.replace($matches[0], $key + ': ' + $value)
-    }
-    if($header -notmatch $meta)
-    {
-      $new_header = $new_header + $key + ': ' + $value + "`r`n"
-    }
-    return $new_header
-  }
-
-  $platyPS_file = $true
-  if((gc $file | Out-String) -match $pattern)
-  {
-    $header = $matches[1]
-    $new_header = $matches[1]
-  }
-  else
-  {
-    $platyPS_file = $false
-    $header = ""
-    $new_header = ""
-  }
-  cd (Split-Path $file -parent)
-
-  $date = (Get-Date (git log --pretty=format:%cd -n 1 --date=iso $file)).ToUniversalTime()
-  $new_header = set_metadata $header $new_header 'updated_at' (Get-Date $date -format g) $true
-  $new_header = set_metadata $header $new_header 'ms.date' (Get-Date $date -format d) $true
-
-  $file_rel_path = $file -replace ".*$root_name", "/$root_name" -replace "\\", "/"
-  $git_prefix = 'https://github.com/' + $env:APPVEYOR_REPO_NAME + '/blob/'
-  $content_git_url = (New-Object System.Uri ($git_prefix + $env:APPVEYOR_REPO_BRANCH + $file_rel_path)).AbsoluteUri
-  $new_header = set_metadata $header $new_header 'content_git_url' $content_git_url  $true
-
-  $git_commit_url = (New-Object System.Uri ($git_prefix + (git rev-list -1 HEAD $file) + $file_rel_path)).AbsoluteUri
-  $new_header = set_metadata $header $new_header 'gitcommit' $git_commit_url  $true
-
-  $topic_type = 'reference'
-  if(!$platyPS_file -or $header -match 'Module\s*Name\s*:')
-  {
-    $topic_type = 'conceptual'
-    $new_header = set_metadata $header $new_header 'uid' ($file_rel_path.split('/',3) | select -Last 1) $true
-  }
-  
-  $new_header = set_metadata $header $new_header 'ms.topic' $topic_type $true
-  $new_header = set_metadata $header $new_header 'ms.prod' $env:prod
-  $new_header = set_metadata $header $new_header 'ms.service' $env:service
-  $new_header = set_metadata $header $new_header 'ms.technology' $env:technology
-  $new_header = set_metadata $header $new_header 'author' $env:author
-  $new_header = set_metadata $header $new_header 'ms.author' ${env:ms.author}
-  $new_header = set_metadata $header $new_header 'keywords' $env:keywords
-  $new_header = set_metadata $header $new_header 'manager' $env:manager
-  if($platyPS_file)
-  {
-    sc $file (gc $file | Out-String).replace($header, ($new_header -replace "{|}", "")) -NoNewline
-  }
-  else
-  {
-    sc $file ("---" + "`r`n" + $new_header + "---" + "`r`n" + (gc $file | Out-String)) -NoNewline
-  }
-
-  if($platyPS_file -and (gc $file | Out-String) -match "#*\s*RELATED\s*LINKS\s*(.|\n)*")
-  {
-    $related_links = $matches[0]
-    $new_related_links = $matches[0]
-    $related_links | sls "\[\S.*\]\(.*\)" -AllMatches | % matches | ? {$_ -match "\(.*.md\s*\)" -and $_ -notmatch "xref:"} | % {
-      $rel_path = (rvpa ($matches[0] -replace "\(|\)", "")) -replace ".*$root_name", "" -replace "\\", "/"
-      $value = "(xref:" + $rel_path.Substring(1, $rel_path.LastIndexOf('/'))
-      $new_related_links = $new_related_links.replace($_, ($_ -replace "\\", "/" -replace "\(.*/", $value))
-    }
-    if($related_links -ne $new_related_links)
-    {
-      sc $file (gc $file | Out-String).replace($related_links, $new_related_links) -NoNewline
-    }
-  }
-}
-function get_toc
+Function GetToc
 {
   if(Test-Path $toc_path)
   {
     rm $toc_path
   }
   ni $toc_path
-  ls $global:root_path -dir | % {do_get_toc $_.FullName 0} 
+  ls $root_path -dir | % {DoGetToc $_.FullName 0} 
   sc $toc_path (gc $toc_path | Out-String).replace("\", "/") -NoNewline
 }
-function global:do_get_toc($folder_path, $level)
+
+Function global:DoGetToc
 {
+  param([string]$folder_path, [int]$level)
+
   $pre = ""
 
   for($i=0;$i -lt $level;$i++)
@@ -114,7 +46,7 @@ function global:do_get_toc($folder_path, $level)
   $index = ls $folder_path | ? {$_.Name -eq 'index.md'} | select -ExpandProperty FullName
   if($index -ne $null)
   {
-    ac $toc_path ($pre + "  href: " + ($index -replace ".*$global:root_name", ".."))
+    ac $toc_path ($pre + "  href: " + ($index -replace ".*$root_name", ".."))
   }
   
   $sub_folders = ls $folder_path -dir
@@ -122,15 +54,15 @@ function global:do_get_toc($folder_path, $level)
   {
     $files = (ls $folder_path) | ? { $_.Extension -eq '.md' } | select -ExpandProperty FullName
     $landing_page = ""
-    $files | ? {(gc $_ | Out-String) -match $pattern -and $matches[1] -match 'Module\s*Name\s*:'} | select -First 1 | % {
-      ac $toc_path ($pre + "  href: " + ($_ -replace ".*$global:root_name", ".."))
+    $files | ? {(gc $_ | Out-String) -match $header_pattern -and $matches[1] -match $landing_page_pattern} | select -First 1 | % {
+      ac $toc_path ($pre + "  href: " + ($_ -replace ".*$root_name", ".."))
       $landing_page = $_
     }
 
     ac $toc_path ($pre + "  items:")
     $pre = $pre + "    "
     $files | ? {$_ -ne $landing_page} | % {
-      ac $toc_path ($pre + "- name: " + (gi $_).BaseName + "`r`n" + $pre + "  href: " + ($_ -replace ".*$global:root_name", ".."))
+      ac $toc_path ($pre + "- name: " + (gi $_).BaseName + "`r`n" + $pre + "  href: " + ($_ -replace ".*$root_name", ".."))
     }
   }
   else
@@ -140,31 +72,132 @@ function global:do_get_toc($folder_path, $level)
     {
       $sub_folders = $sub_folders | sort -Property Name -Descending
     }
-    $sub_folders | % {do_get_toc $_.FullName ($level + 1)}
+    $sub_folders | % {DoGetToc $_.FullName ($level + 1)}
   }
 }
 
-$MaxThreads = 8
-$RunspacePool = [RunspaceFactory ]::CreateRunspacePool(1, $MaxThreads)
-$RunspacePool.Open()
-$Jobs = @()
-$files = ls $global:root_path -r | ? {$_.extension -eq '.md'} | % { $_.FullName }
-$files | % {
-  $Job = [powershell]::Create().AddScript($script_block).AddArgument($_).AddArgument($global:root_name).AddArgument($pattern)
-  $Job.RunspacePool = $RunspacePool
-  $Jobs += New-Object PSObject -Property @{
-    RunNum = $_
-    Pipe = $Job
-    Result = $Job.BeginInvoke()
-  }
-}
-Write-Host "Processing files..."
-Do
+$script_block =
 {
-  sleep -Seconds 1
-} While ($Jobs.Result.IsCompleted -contains $false)
-Write-Host "Process files completed!"
+  param([string]$file, [string]$root_name, [string]$pattern, [string]$landing_page_pattern)
 
-Write-Host "constructing toc..."
-get_toc
-Write-Host "constructing toc completed."
+  $related_link_pattern = "#*\s*RELATED\s*LINKS\s*(.|\n)*"
+  $platyPS_file = $true
+
+  Function SetMetadata
+  {
+    param([string]$header, [string]$new_header, [string]$key, [string]$value, [bool]$overwrite)
+
+    $meta = "(?m)^$key\s*:[\s\S].*"
+    if($header -match $meta -and $overwrite)
+    {
+      $new_header = $new_header.replace($matches[0], $key + ': ' + $value)
+    }
+    if($header -notmatch $meta)
+    {
+      $new_header = $new_header + $key + ': ' + $value + "`r`n"
+    }
+    return $new_header
+  }
+
+  if((gc $file | Out-String) -match $pattern)
+  {
+    $header = $matches[1]
+    $new_header = $matches[1]
+  }
+  else
+  {
+    $platyPS_file = $false
+    $header = ""
+    $new_header = ""
+  }
+  # need to get git log info and resolve relative path
+  cd (Split-Path $file -parent)
+
+  # set or update metadata in the header of .md files
+  $date = (Get-Date (git log --pretty=format:%cd -n 1 --date=iso $file)).ToUniversalTime()
+  $new_header = SetMetadata $header $new_header 'updated_at' (Get-Date $date -format g) $true
+  $new_header = SetMetadata $header $new_header 'ms.date' (Get-Date $date -format d) $true
+
+  $file_rel_path = $file -replace ".*$root_name", "/$root_name" -replace "\\", "/"
+  $git_prefix = 'https://github.com/' + $env:APPVEYOR_REPO_NAME + '/blob/'
+  $content_git_url = (New-Object System.Uri ($git_prefix + $env:APPVEYOR_REPO_BRANCH + $file_rel_path)).AbsoluteUri
+  $new_header = SetMetadata $header $new_header 'content_git_url' $content_git_url  $true
+
+  $git_commit_url = (New-Object System.Uri ($git_prefix + (git rev-list -1 HEAD $file) + $file_rel_path)).AbsoluteUri
+  $new_header = SetMetadata $header $new_header 'gitcommit' $git_commit_url  $true
+
+  $topic_type = 'reference'
+  if(!$platyPS_file -or $header -match $landing_page_pattern)
+  {
+    $topic_type = 'conceptual'
+    $new_header = SetMetadata $header $new_header 'uid' ($file_rel_path.split('/',3) | select -Last 1) $true
+  }
+  
+  $new_header = SetMetadata $header $new_header 'ms.topic' $topic_type $true
+  $new_header = SetMetadata $header $new_header 'ms.prod' $env:prod
+  $new_header = SetMetadata $header $new_header 'ms.service' $env:service
+  $new_header = SetMetadata $header $new_header 'ms.technology' $env:technology
+  $new_header = SetMetadata $header $new_header 'author' $env:author
+  $new_header = SetMetadata $header $new_header 'ms.author' ${env:ms.author}
+  $new_header = SetMetadata $header $new_header 'keywords' $env:keywords
+  $new_header = SetMetadata $header $new_header 'manager' $env:manager
+
+  if($platyPS_file)
+  {
+    # reduce unnecessary file write
+    if($header -ne $new_header)
+    {
+      sc $file (gc $file | Out-String).replace($header, ($new_header -replace "{|}", "")) -NoNewline
+    }
+
+    # resolve related links to the format that docfx supports [link name](xref:uid)
+    if((gc $file | Out-String) -match $related_link_pattern)
+    {
+      $related_links = $matches[0]
+      $new_related_links = $matches[0]
+      $related_links | sls "\[\S.*\]\(.*\)" -AllMatches | % matches | ? {$_ -match "\(.*.md\s*\)" -and $_ -notmatch "xref:"} | % {
+        $rel_path = (rvpa ($matches[0] -replace "\(|\)", "")) -replace ".*$root_name", "" -replace "\\", "/"
+        $value = "(xref:" + $rel_path.Substring(1, $rel_path.LastIndexOf('/'))
+        $new_related_links = $new_related_links.replace($_, ($_ -replace "\\", "/" -replace "\(.*/", $value))
+      }
+    }
+    if($related_links -ne $new_related_links)
+    {
+      sc $file (gc $file | Out-String).replace($related_links, $new_related_links) -NoNewline
+    }
+  }
+  else
+  {
+    sc $file ("---" + "`r`n" + $new_header + "---" + "`r`n" + (gc $file | Out-String)) -NoNewline
+  }
+}
+Function ProcessFiles
+{
+  param([int]$max_threads)
+  $RunspacePool = [RunspaceFactory ]::CreateRunspacePool(1, $max_threads)
+  $RunspacePool.Open()
+  $Jobs = @()
+  ls $root_path -r | ? {$_.extension -eq '.md'} | % {
+    $Job = [powershell]::Create().AddScript($script_block).AddArgument($_.FullName).AddArgument($root_name).AddArgument($header_pattern).AddArgument($landing_page_pattern)
+    $Job.RunspacePool = $RunspacePool
+    $Jobs += New-Object PSObject -Property @{
+      RunNum = $_.FullName
+      Pipe = $Job
+      Result = $Job.BeginInvoke()
+    }
+  }
+  Do
+  {
+    sleep -Seconds 1
+  } While ($Jobs.Result.IsCompleted -contains $false)
+}
+
+# Step 1: process .md files
+echo "Process files ..."
+ProcessFiles 8
+
+# Step 2: generate toc.yml
+echo "generate toc..."
+GetToc
+
+echo "completed successfully."
--- update_content_repo.ps1
+++ update_content_repo.ps1
@@ -1,71 +1,110 @@
-# 1. copy docs to processing repo
-# 2. copy toc and index if there is any
-# 3. update global toc and breadcrumb if necessary
+<#
+.SYNOPSIS
+    This is a Powershell script to copy files and update breadcrumb.json if necessary
+.DESCRIPTION
+    This script is used in specific ci projects(appveyor.yml) and depending on both
+    APPVEYOR built-in environment variables and the ones defined in those projects.
+    We didn't decouple it cause we want to keep the update as more as possible in this
+    script instead of in the appveyor.yml.
+#>
+
+# Validate that the repo has only one root folder
+if((ls $env:APPVEYOR_BUILD_FOLDER -dir).count -ne 1)
+{
+  $host.SetShouldExit(-1)
+}
 
 $ErrorActionPreference = 'Stop'
-ls $global:root_path -dir | % {
-  $target = Join-Path $env:TEMP\Azure $env:target_folder | Join-Path -ChildPath (Split-Path $_.FullName -Leaf)
-  if(Test-Path $target)
-  {
-    rm $target -Recurse -Force
-  }
-  copy $_.FullName $target -Recurse -Force
-}
 
-$toc_folder = Join-Path $env:TEMP\Azure $env:target_folder\$global:root_name
-if(!(Test-Path $toc_folder))
-{
-  ni $toc_folder -type Directory
-}
+$root_path = (ls $env:APPVEYOR_BUILD_FOLDER -dir | select -First 1).FullName
+$root_name = Split-Path $root_path -Leaf
+$target_path = Join-Path $env:TEMP\Azure $env:target_folder
 
-copy (Join-Path $global:root_path "toc.yml") $toc_folder
-$index = Join-Path $global:root_path "index.md"
-if(Test-Path $index)
+Function CopyFiles
 {
-  copy $index $toc_folder
-}
-$global_toc = Join-Path $env:TEMP\Azure $env:target_folder\toc.yml
-if(!(Test-Path $global_toc))
-{
-  ni $global_toc
-}
-if((gc $global_toc | Out-String) -notmatch $global:root_name)
-{
-  ac $global_toc ("- name: " + $global:root_name)
-  if(Test-Path $index)
-  {
-    ac $global_toc ("  href: " + $global:root_name + "/index.md")
-  }
-  ac $global_toc ("  tocHref: " + $global:root_name + "/toc.yml")
-}
-$breadcrumb_path = Join-Path $env:TEMP\Azure $env:target_folder\breadcrumb.json
-$breadcrumb = (gc -Raw $breadcrumb_path) | ConvertFrom-Json
-$children = $breadcrumb.children
-if($children -ne $null)
-{
-  $new_node = $true
-  $children.children | ? {$_.href -match $global:root_name} | select -First 1 | % {$new_node = $false}
-  if($new_node)
-  {
-    $new_child = New-Object PSObject -Property @{
-      href = $children[0].href + $global:root_name + "/"
-      homepage = $children[0].href + $global:root_name + "/"
-      toc_title = $global:root_name
-      level = 3
-    }
-    
-    if($children.children -eq $null)
+  ls $root_path -dir | % {
+    $target = Join-Path $target_path (Split-Path $_.FullName -Leaf)
+    if(Test-Path $target)
     {
-      $children | Add-Member -MemberType NoteProperty -Name children -value @($new_child)
+      rm $target -Recurse -Force
     }
-    else
-    {
-      $children.children += $new_child
-    }
+    copy $_.FullName $target -Recurse -Force
+  }
 
-    Write-Host 'update breadcrumb...'
-    # powershell read json array(one object array) issue, have to insert array mark manually
-    sc $breadcrumb_path ('[' + ($breadcrumb | ConvertTo-Json -Depth 5) + ']') -NoNewline
-    Write-Host 'update breadcrumb completed.'
+  $toc_folder = Join-Path $target_path $root_name
+  if(!(Test-Path $toc_folder))
+  {
+    ni $toc_folder -type Directory
+  }
+
+  copy (Join-Path $root_path "toc.yml") $toc_folder
+
+  if(Join-Path $root_path "index.md" | Test-Path)
+  {
+    copy (Join-Path $root_path "index.md") $toc_folder
   }
 }
+
+Function UpdateGlobalToc
+{
+  param([string]$global_toc)
+  if(!(Test-Path $global_toc))
+  {
+    ni $global_toc
+  }
+  if((gc $global_toc | Out-String) -notmatch $root_name)
+  {
+    ac $global_toc ("- name: " + $root_name)
+    if(Join-Path $root_path "index.md" | Test-Path)
+    {
+      ac $global_toc ("  href: " + $root_name + "/index.md")
+    }
+    ac $global_toc ("  tocHref: " + $root_name + "/toc.yml")
+  }
+}
+
+Function UpdateBreadcrumb
+{
+  param([string]$breadcrumb_path)
+
+  $breadcrumb = (gc -Raw $breadcrumb_path) | ConvertFrom-Json
+  $children = $breadcrumb.children
+  if($children -ne $null)
+  {
+    $new_node = $true
+    $children.children | ? {$_.href -match $root_name} | select -First 1 | % {$new_node = $false}
+    if($new_node)
+    {
+      $new_child = New-Object PSObject -Property @{
+        href = $children[0].href + $root_name + "/"
+        homepage = $children[0].href + $root_name + "/"
+        toc_title = $root_name
+        level = 3
+      }
+      if($children.children -eq $null)
+      {
+        $children | Add-Member -MemberType NoteProperty -Name children -value @($new_child)
+      }
+      else
+      {
+        $children.children += $new_child
+      }
+      # powershell read json array(one object array) issue, have to insert array mark manually
+      sc $breadcrumb_path ('[' + ($breadcrumb | ConvertTo-Json -Depth 5) + ']') -NoNewline
+    }
+  }
+}
+
+# Step 1. copy docs, toc and index page(if there is any) to processing repo
+echo "copy files ..."
+CopyFiles
+
+# Step 2: add current project to toc node if necessary
+echo "update global toc ..."
+UpdateGlobalToc (Join-Path $target_path "toc.yml")
+
+# Step 3. update breadcrumb if necessary
+echo "update breadcrumb ..."
+UpdateBreadcrumb (Join-Path $target_path "breadcrumb.json")
+
+echo "completed successfully."
